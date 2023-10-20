01a519a5f2390b86787463eb3779077d70e8d10c
Farvaev Emil <farvaev.emil@gmail.com>
2023-10-20 20:54:56 +0300

    cleanup + setup open sans font

---
+++ src/app/fonts.ts
@@ -0,0 +1,7 @@
+import { Open_Sans } from "next/font/google";
+
+export const openSans = Open_Sans({
+  subsets: ["latin", "cyrillic", "cyrillic-ext"],
+  weight: ["400", "700"],
+  style: ["italic", "normal"],
+});
