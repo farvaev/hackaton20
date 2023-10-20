e35f396abd9ec6cb5fe1f551ddec5c82a9df514e
Farvaev Emil <farvaev.emil@gmail.com>
2023-10-20 20:17:58 +0300

    add colors to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        MainBlue: "#264B82",
+        GrayBlue: "#D4DBE6",
+        PaleBlue: "#E7EEF7",
+        Red: "#DB2B21",
+        Black: "#1A1A1A",
+        PaleBlack: "#8C8C8C",
+        Green: "#0D9B68",
+        SeaDark: "#167C85",
+        SeaClear: "#48B8C2",
+        Orange: "#F67319",
+        Gray: "#7F8792",
+        LightGray: "#C9D1DF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
