41cfc6bb243e50a31012b70a5c1d20080366e913
Farvaev Emil <farvaev.emil@gmail.com>
2023-10-22 09:39:30 +0300

    styles

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -1,14 +1,13 @@
-"use client"
+"use client";
 
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Categories } from "./Categories"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Categories } from "./Categories";
 
 export default function ManagerPage() {
-
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col max-w-3xl m-auto">
       <Categories />
     </div>
-  )
+  );
 }
--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -5,7 +5,7 @@ export type InputProps = React.HTMLProps<HTMLInputElement>;
 export default function Input(props: InputProps) {
   return (
     <input
-      className="p-2 border border-solid border-Black rounded-sm inline-block"
+      className="px-2 py-1 border border-solid border-Black/50 focus-within:border-Black rounded-sm inline-block"
       {...props}
     />
   );
